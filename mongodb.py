5ca507eb87f33527d9cf7da0218c942d6c561981
Sanjyuktha <sanjyuktha.d.2024.it@rajalakshmi.edu.in>
2026-06-30 14:39:38 +0530

    Update mongodb.py

--- mongodb.py
+++ mongodb.py
@@ -2,8 +2,7 @@ import os
 from pymongo import MongoClient
 
 # 1. Safely look up the variable name we will give to Streamlit Cloud
-mongo_uri = os.getenv("mongodb+srv://sanjyukthad2024it_db_user:suns09@cluster0.ddrvj4p.mongodb.net/?appName=Cluster0")
-
+mongo_uri = os.getenv("MONGO_URI")
 # 2. Pass that correct variable name directly into the client
 client = MongoClient(mongo_uri)
 
